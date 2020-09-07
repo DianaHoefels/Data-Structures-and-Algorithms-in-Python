2afa0d63339005a6452f84eff265b58e1fb03c9e
DianaHoefels <diana-constantina.hoefels@student.uni-tuebingen.de>
2020-09-07 16:48:56 +0200

    Projects

--- Chapter 01 Python Primer/Projects/P.1.29.py
+++ Chapter 01 Python Primer/Projects/P.1.29.py
@@ -4,22 +4,22 @@ import itertools
 
 # recursively
 
-def permutate_2(start, characters=''):
+def permutate_1(start, characters=''):
     if len(start) == 0:
         print(characters)
     else:
         for i in range(len(start)):
-            permutate_2(start[:i] + start[i+1:], characters + start[i])
+            permutate_1(start[:i] + start[i+1:], characters + start[i])
+# or with the built-in permutation function from itertools library.
 
-def permutate_1(characters):
+def permutate_2(characters):
 
     all_strings = list(itertools.permutations(characters))
 
     return ["".join(character) for character in all_strings]
 
-# or with the built-in permutation function from itertools library.
 
 if __name__ == '__main__':
-    #print(permutate_1(['c','a', 't', 'd', 'o', 'g']))
     sequence_characters = ['c','a', 't', 'd', 'o', 'g']
+    print(permutate_1(sequence_characters))
     print(permutate_2(sequence_characters))
